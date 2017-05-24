81418f7ca103458ef07cab4d8cef62e9346c37a0
Angelique Barth <angeliqueb71@aol.com>
2017-05-23 23:45:33 -0400

    styled home.html and home.scss

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,6 +2,6 @@ class RoomsController < ApplicationController
   before_action :authenticate_user!
   def show
     @messages = Message.all
-    
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+  get '/haircare' => 'haircare#index'
+
+  get '/chemicals' => 'chemicals#index'
+
+  get '/color' => 'color#index'
+  get '/rooms' => 'rooms#show'
+  resources :messages
+
+
+
   devise_for :users
-  root to: 'rooms#show'
+  root to: 'home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount ActionCable.server => '/cable'
