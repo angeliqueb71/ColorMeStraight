358aed1ced7f22a19b2b70450d900a8cb03a1468
Angelique Barth <angeliqueb71@aol.com>
2017-06-06 16:34:46 -0400

    changed image_tag

--- config/application.rb
+++ config/application.rb
@@ -8,10 +8,9 @@ Bundler.require(*Rails.groups)
 
 module ColorMEStraight
   class Application < Rails::Application
+    config.serve_static_files = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
 end
-
-config.serve_static_files = true
